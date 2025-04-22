830ebf33cb3d0225060e2a4da67be33c65e27e65
Pedro <lucaspires2322@gmail.com>
2025-04-22 20:50:24 -0300

    feat: alterando cores da aplicação

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,29 +1,41 @@
 export const darkTheme = {
-    'background': '#1B4332',
+    'background': '#131515',
+    'background-card': '#212529',
+    'sub-background': '#343a40',
+    
+    'text-project': '#D8F3DC',  
+    'skill-background': '#339989',
+    'text-skill': '#D8F3DC',
+    
     'background-arrow': '#CED4DA',
-    'background-card': '#081C15',
-    'sub-background': '#212529',
-  
-    'skill-background': '#40916C',
+    'border-arrow': '#CED4DA',
+
+    'scroll-track':'#D8F3DC',
+    'scroll-thumb':'#131515',
 
     'text': '#D8F3DC',
-    'text-light': '#D8F3DC',
 
-    'toggle': '#40916C',
+    'toggle': '#adb5bd',
 
-    'background-menu' : '#081C15' 
+    'background-menu' : '#2d6a4f' 
 }
 
 export const lightTheme = {
-    'background': '#40916C',
-    'background-arrow': '#CED4DA',
-    'background-card': '#D8F3DC',
-    'sub-background': '#212529',
+    'background': '#e9ecef',
+    'background-card': '#ced4da',
+    'sub-background': '#adb5bd',
+    
+    'text-project': '#333333',
+    'skill-background': '#6b9080',
+    'text-skill': '#D8F3DC',
 
-    'skill-background': '#40916C',
+    'background-arrow': '#transparent',
+    'border-arrow': '#333333',
+
+    'scroll-track':'#e9ecef',
+    'scroll-thumb':'#333333',
 
     'text': '#333333',
-    'text-light': '#D8F3DC',
 
     'toggle': '#1B4332',
 
