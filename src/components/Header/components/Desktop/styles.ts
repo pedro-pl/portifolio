eeda2749195651217fe5078d5a9da0a79db02e90
Pedro <lucaspires2322@gmail.com>
2025-05-01 20:08:11 -0300

    fix: corrigindo comportamento

--- src/components/Header/components/Desktop/styles.ts
+++ src/components/Header/components/Desktop/styles.ts
@@ -6,6 +6,7 @@ export const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    user-select: none;
 
     h3 {
         color: ${props => props.theme['text']};
