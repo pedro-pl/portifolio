389de3d2db07c07187a3e08ba79f6bd27245860f
Pedro <lucaspires2322@gmail.com>
2025-05-02 13:37:22 -0300

    feat: adicionando projeto

--- src/mocks/projects/index.ts
+++ src/mocks/projects/index.ts
@@ -1,3 +1,4 @@
+import Portfolio from '../../assets/portfolio.png';
 import TimerPomodoro from '../../assets/timer-pomodoro.png';
 import IgniteFeed from '../../assets/ignite-feed.png';
 import ToDoList from '../../assets/to-do-list.png';
@@ -5,6 +6,22 @@ import ToDoList from '../../assets/to-do-list.png';
 export const projects = [
     {
         id: 0,
+        name: "PORTFOLIO",
+        skills: [
+            "React",
+            "TypeScript",
+            "JavaScript",
+            "Styled Components",
+            "CSS"
+        ],
+        img: Portfolio,
+        descriptionPt: "Desenvolvi este projeto do zero, utilizando os conhecimentos que adquiri até o momento. Ele poderia ter sido feito de uma maneira bem simples, mas escolhi aplicar tudo o que aprendi para torná-lo mais organizado e legível. Através deste projeto, quero demonstrar minhas habilidades e evolução como desenvolvedor.",
+        descriptionEn: "I developed this project from scratch, using all the knowledge I've acquired so far. It could have been done in a much simpler way, but I chose to apply everything I've learned to make it more organized and readable. Through this project, I want to showcase my skills and growth as a developer.",
+        linkGit: "https://github.com/pedro-pl/portfolio.git",
+        linkWeb: "https://portfolio-eight-flame-68.vercel.app/"
+    },
+    {
+        id: 1,
         name: "TIMER POMODORO",
         skills: [
             "React",
@@ -20,7 +37,7 @@ export const projects = [
         linkWeb: "https://timer-pomodoro-omega.vercel.app/"
     },
     {
-        id: 1,
+        id: 2,
         name: "TO DO LIST",
         skills: [
             "React",
@@ -35,7 +52,7 @@ export const projects = [
         linkWeb: "https://to-do-list-iota-beige.vercel.app/"
     },
     {
-        id: 2,
+        id: 3,
         name: "IGNITE FEED",
         skills: [
             "React",
